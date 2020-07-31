667255779d3055ea14af14c756ff19ea3875fb6f
sho.tanaka <sho.tanaka@aijus.com>
2020-07-31 19:35:33 +0900

    done

---
+++ src/features/todos/AddTodo.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from 'features/todos/todosSlice'
+
+const mapDispatch = { addTodo }
+
+const AddTodo = ({ addTodo }) => {
+  const [todoText, setTodoText] = useState('')
+  const onChange = (e) => setTodoText(e.target.value)
+  React.useEffect(() => {
+    console.log('mapDispatch', mapDispatch)
+    console.log('mapDispatch', todoText)
+  })
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          if (!todoText.trim()) {
+            return
+          }
+          addTodo(todoText)
+          setTodoText('')
+        }}
+      >
+        <input value={todoText} onChange={onChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  )
+}
+
+export default connect(null, mapDispatch)(AddTodo)
